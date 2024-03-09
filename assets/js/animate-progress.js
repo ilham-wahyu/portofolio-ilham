c9c8a6e4b2598544369660e5da302e0a7cef0499
ilham-cuy1 <ilhamwahyu9784@gmail.com>
2024-03-09 21:15:10 +0700

    Update

---
+++ assets/js/animate-progress.js
@@ -0,0 +1,23 @@
+$(".progress-bar-html").animate({ 
+    width: "90%", 
+}, 1700);
+
+$(".progress-bar-css").animate({ 
+    width: "85%", 
+}, 1700);
+
+$(".progress-bar-php").animate({ 
+    width: "80%", 
+}, 1700);
+
+$(".progress-bar-js").animate({ 
+    width: "75%", 
+}, 1700);
+
+$(".progress-bar-laravel").animate({ 
+    width: "90%", 
+}, 1700);
+
+$(".progress-bar-ci").animate({ 
+    width: "80%", 
+}, 1700);
